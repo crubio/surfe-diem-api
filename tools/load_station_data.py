4ac43db33eb617c940384b6aa72c170eacf16726
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-06-15 15:41:24 -0700

    updating db url

--- tools/load_station_data.py
+++ tools/load_station_data.py
@@ -6,18 +6,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-conn = psycopg2.connect(
-   database=os.environ.get('DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'),
-   host=os.environ.get('DATABASE_HOSTNAME'), port=os.environ.get('DATABASE_PORT')
-)
+# conn = psycopg2.connect(
+#    database=os.environ.get('DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'),
+#    host=os.environ.get('DATABASE_HOSTNAME'), port=os.environ.get('DATABASE_PORT')
+# )
+conn = psycopg2.connect(database_url=os.environ.get('DATABASE_URL'))
 
 # from sqlalchemy import create_engine - this is a working example of how to connect to the database
 # SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 def import_locations(json_data_file):
-    pass
     '''runs the import from our json file'''
+    if not json_data_file:
+        raise Exception('No json file specified')
     cursor = conn.cursor()
     with open(json_data_file) as data_file:
         data = json.load(data_file)
