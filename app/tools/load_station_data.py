698062aeae2a4bfc35675b18a8817d2c8a4558b4
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-06-16 18:08:53 -0700

    enable ssl req

--- app/tools/load_station_data.py
+++ app/tools/load_station_data.py
@@ -6,12 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# conn = psycopg2.connect(
-#    database=os.environ.get('DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'),
-#    host=os.environ.get('DATABASE_HOSTNAME'), port=os.environ.get('DATABASE_PORT')
-# )
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(
+   database=os.environ.get('DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'),
+   host=os.environ.get('DATABASE_HOSTNAME'), port=os.environ.get('DATABASE_PORT'), sslmode='require'
+)
 
 # from sqlalchemy import create_engine - this is a working example of how to connect to the database
 # SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
@@ -42,6 +40,6 @@ def import_locations_alt(json_data_file):
     '''runs the import from our json file'''
     if not json_data_file:
         raise Exception('No json file specified')
-    pass
+    
 if __name__ == '__main__':
     import_locations(sys.argv[1])
