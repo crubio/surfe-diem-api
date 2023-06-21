1b5f13ce7a1ad3a6eb42ea0783048709aa102b6e
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-06-21 15:26:45 -0700

    remove unused files

--- test_run.py
+++
@@ -1,9 +0,0 @@
-import app.utils.test_db_conn as test_db_conn
-
-def main():
-    '''testing running db connection from main'''
-    test_db_conn.test()
-    return
-
-if __name__ == '__main__':
-    main()
