3bf4d9fdf676f3fbb4f00fff4ba589944d23db77
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-07-23 10:30:10 -0700

    quick fix to auth module 404 error return

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,9 +12,8 @@ router = APIRouter(
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(
         models.User.email == user_credentials.username).first()
-    print(user)
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_NOT_FOUND, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
