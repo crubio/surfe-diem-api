401fa5a462adcc0427df2d5906680a036eef39e7
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2023-07-31 17:04:13 -0700

    update cleanup job

--- tools/delete_old_summaries.py
+++ tools/delete_old_summaries.py
@@ -12,6 +12,15 @@ conn = psycopg2.connect(
    host=os.environ.get('DATABASE_HOSTNAME'), port=os.environ.get('DATABASE_PORT')
 )
 
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting delete_old_summaries.py")
+    delete_old_summaries()
+    delete_old_latest_observation()
+    conn.close()
+    logging.info("Finished delete_old_summaries.py")
+    return
+
 def delete_old_summaries():
     '''deletes summaries older than 48 hours'''
     cursor = conn.cursor()
@@ -23,8 +32,20 @@ def delete_old_summaries():
     except Exception as error:
         logging.warning(f"Error deleting rows: {error}")
         conn.rollback()
-    conn.close()
+    return
+
+def delete_old_latest_observation():
+    '''deletes latest observation older than 48 hours'''
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''DELETE FROM locations_latest_observation WHERE date_created < NOW() - INTERVAL '48 hours';''')
+        conn.commit()
+        logging.info(f"Deleted {cursor.rowcount} rows")
+        print(f"Deleted {cursor.rowcount} rows")
+    except Exception as error:
+        logging.warning(f"Error deleting rows: {error}")
+        conn.rollback()
     return
 
 if __name__ == '__main__':
-    delete_old_summaries()
+    main()
